d03842e3c219f7ea0bf16afda97f7657f2c1f5d4
lorenzodecilladi <lorenzo.decilladi@edu.unito.it>
2017-08-14 15:14:48 +0200

    minor

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,28 @@
 //jhvxjhvhvhxahlkjsbkjbx vzxhb
+
+
+#include <iostream>
+
+void sayHello(const char*);
+
+int main(){
+
+  char name[100];
+  cin >> name;
+  cout << Hello
+}
+
+
+
+
+
+jbkjev;
+
+
+
+cewc;
+q;
+
+
+
+eqcqwdcsxzxd;
